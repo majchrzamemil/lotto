d94d81b97fd8c69635e97efb21c5b56cc6b2dfea
Emil Majchrzak <majchrzakemil@gitlab.com>
2021-05-07 18:17:13 +0200

    Added teplate for gable and formatted
    Signed-off-by: Emil Majchrzak <majchrzakemil@gitlab.com>

--- include/lotto.hpp
+++ include/lotto.hpp
@@ -5,39 +5,40 @@
 
 namespace eosio {
 
-  using std::string;
-  class [[eosio::contract("lotto")]] lotto : public contract {
-    public:
-      using contract::contract;
+using std::string;
+class [[eosio::contract("lotto")]] lotto : public contract {
+ public:
+  using contract::contract;
 
-      [[eosio::action]] 
-      void create(const name& issuer, const asset& maximum_supply);
+  [[eosio::action]]
+  void create(const name& issuer, const asset& maximum_supply);
 
-      [[eosio::action]]
-      void issue( const name& to, const asset& quantity, const string& memo );
-
-    private:
-      struct [[eosio::table]] account {
-        asset balance;
-        uint64_t primary_key() const {
-          return balance.symbol.code().raw();
-        }
-      };
-
-      struct [[eosio::table]] currency_stats {
-        asset supply;
-        asset max_supply;
-        name issuer;
-        uint64_t primary_key() const {
-          return supply.symbol.code().raw();
-        }
-      };
-
-      typedef eosio::multi_index< "accounts"_n, account > accounts;
-      typedef eosio::multi_index< "stat"_n, currency_stats > stats;
-   
-      void sub_balance( const name& owner, const asset& value );
-      void add_balance( const name& owner, const asset& value, const name& ram_payer );
+  [[eosio::action]]
+  void issue(const name& to, const asset& quantity, const string& memo);
+  [[eosio::action]]
+  void gamble(const name& from, const asset&   quantity,       const string&  memo);
+ private:
+  struct [[eosio::table]] account {
+    asset balance;
+    uint64_t primary_key() const {
+      return balance.symbol.code().raw();
+    }
   };
+
+  struct [[eosio::table]] currency_stats {
+    asset supply;
+    asset max_supply;
+    name issuer;
+    uint64_t primary_key() const {
+      return supply.symbol.code().raw();
+    }
+  };
+
+  typedef eosio::multi_index< "accounts"_n, account > accounts;
+  typedef eosio::multi_index< "stat"_n, currency_stats > stats;
+
+  void sub_balance(const name& owner, const asset& value);
+  void add_balance(const name& owner, const asset& value, const name& ram_payer);
+};
 }
 
--- src/lotto.cpp
+++ src/lotto.cpp
@@ -5,68 +5,87 @@ namespace eosio {
 void lotto::create(const name& issuer, const asset& maximum_supply) {
   require_auth(get_self());
   auto sym = maximum_supply.symbol;
-  check( sym.is_valid(), "invalid symbol name" );
-  check( maximum_supply.is_valid(), "invalid supply");
-  check( maximum_supply.amount > 0, "max-supply must be positive");
+  check(sym.is_valid(), "invalid symbol name");
+  check(maximum_supply.is_valid(), "invalid supply");
+  check(maximum_supply.amount > 0, "max-supply must be positive");
 
-  stats statstable( get_self(), sym.code().raw() );
+  stats statstable(get_self(), sym.code().raw());
 
-  statstable.emplace( get_self(), [&]( auto& s ) {
-     s.supply.symbol = maximum_supply.symbol;
-     s.max_supply    = maximum_supply;
-     s.issuer        = issuer;
+  statstable.emplace(get_self(), [&](auto & s) {
+    s.supply.symbol = maximum_supply.symbol;
+    s.max_supply    = maximum_supply;
+    s.issuer        = issuer;
   });
 }
 
-void lotto::issue( const name& to, const asset& quantity, const string& memo )
-{
-    auto sym = quantity.symbol;
-    check( sym.is_valid(), "invalid symbol name" );
-    check( memo.size() <= 256, "memo has more than 256 bytes" );
+//TODO:Maybe fuck issue, asset can be removed and made const its only constexpr uint64_t
+void lotto::issue(const name& to, const asset& quantity, const string& memo) {
+  auto sym = quantity.symbol;
+  check(sym.is_valid(), "invalid symbol name");
+  check(memo.size() <= 256, "memo has more than 256 bytes");
 
-    stats statstable( get_self(), sym.code().raw() );
-    auto existing = statstable.find( sym.code().raw() );
-    check( existing != statstable.end(), "token with symbol does not exist, create token before issue" );
-    const auto& st = *existing;
-    //check( to == st.issuer, "tokens can only be issued to issuer account" );
+  stats statstable(get_self(), sym.code().raw());
+  auto existing = statstable.find(sym.code().raw());
+  check(existing != statstable.end(), "token with symbol does not exist, create token before issue");
+  const auto& st = *existing;
 
-    //require_auth( st.issuer );
-    check( quantity.is_valid(), "invalid quantity" );
-    check( quantity.amount > 0, "must issue positive quantity" );
+  check(quantity.is_valid(), "invalid quantity");
+  check(quantity.amount > 0, "must issue positive quantity");
 
-    check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
-    check( quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
+  check(quantity.symbol == st.supply.symbol, "symbol precision mismatch");
+  check(quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
 
-    statstable.modify( st, same_payer, [&]( auto& s ) {
-       s.supply += quantity;
+  statstable.modify(st, same_payer, [&](auto & s) {
+    s.supply += quantity;
+  });
+
+  add_balance(to, quantity, st.issuer);
+}
+
+//TODO:Change to have const quantity
+void lotto::gamble(const name& from, const asset&   quantity, const string&  memo) {
+  require_auth(from);
+  check(is_account(to), "to account does not exist");
+  auto sym = quantity.symbol.code();
+  stats statstable(get_self(), sym.raw());
+  const auto& st = statstable.get(sym.raw());
+
+  require_recipient(from);
+
+  check(quantity.is_valid(), "invalid quantity");
+  check(quantity.amount > 0, "must transfer positive quantity");
+  check(quantity.symbol == st.supply.symbol, "symbol precision mismatch");
+  check(memo.size() <= 256, "memo has more than 256 bytes");
+
+  //TODO: add some table for gamble, and than gamble with it.
+  //at the end sub and add appropraite balance.
+  //sub_balance(from, quantity);
+
+  //add_balance(to, quantity, payer );
+}
+
+void lotto::sub_balance(const name& owner, const asset& value) {
+  accounts from_acnts(get_self(), owner.value);
+
+  const auto& from = from_acnts.get(value.symbol.code().raw(), "no balance object found");
+  check(from.balance.amount >= value.amount, "overdrawn balance");
+
+  from_acnts.modify(from, owner, [&](auto & a) {
+    a.balance -= value;
+  });
+}
+
+void lotto::add_balance(const name& owner, const asset& value, const name& ram_payer) {
+  accounts to_acnts(get_self(), owner.value);
+  auto to = to_acnts.find(value.symbol.code().raw());
+  if (to == to_acnts.end()) {
+    to_acnts.emplace(ram_payer, [&](auto & a) {
+      a.balance = value;
     });
-
-    add_balance( to, quantity, st.issuer );
-}
-
-void lotto::sub_balance( const name& owner, const asset& value ) {
-   accounts from_acnts( get_self(), owner.value );
-
-   const auto& from = from_acnts.get( value.symbol.code().raw(), "no balance object found" );
-   check( from.balance.amount >= value.amount, "overdrawn balance" );
-
-   from_acnts.modify( from, owner, [&]( auto& a ) {
-         a.balance -= value;
-      });
-}
-
-void lotto::add_balance( const name& owner, const asset& value, const name& ram_payer )
-{
-   accounts to_acnts( get_self(), owner.value );
-   auto to = to_acnts.find( value.symbol.code().raw() );
-   if( to == to_acnts.end() ) {
-      to_acnts.emplace( ram_payer, [&]( auto& a ){
-        a.balance = value;
-      });
-   } else {
-      to_acnts.modify( to, same_payer, [&]( auto& a ) {
-        a.balance += value;
-      });
-   }
+  } else {
+    to_acnts.modify(to, same_payer, [&](auto & a) {
+      a.balance += value;
+    });
+  }
 }
 }
